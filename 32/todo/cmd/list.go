37977753c70fc0457f88f5b6bbf02fd392ee9ac8
Manuel Tiago Pereira <mt.pereira@gmail.com>
2018-04-09 22:18:36 +0100

    WIP.

---
+++ 32/todo/cmd/list.go
@@ -0,0 +1,65 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/campoy/justforfunc/31-grpc/todo"
+
+	"github.com/spf13/cobra"
+	grpc "google.golang.org/grpc"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all of the tasks.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conn, err := grpc.Dial(":8888", grpc.WithInsecure())
+		if err != nil {
+			return fmt.Fprintf(os.Stderr, "could not connect to backend")
+			os.Exit(1)
+		}
+		client := todo.NewTasksClient(conn)
+
+		return list(context.Background(), client)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+func list(ctx context.Context, filterDone bool) error {
+	l, err := client.List(ctx, &todo.Void{})
+	if err != nil {
+		return fmt.Errorf("could not fetch tasks: %v", err)
+	}
+	for _, t := range l.Tasks {
+		if t.Done {
+			if filterDone {
+				continue
+			}
+			fmt.Printf("👍")
+		} else {
+			fmt.Printf("😱")
+		}
+		fmt.Printf(" %s\n", t.Text)
+	}
+	return nil
+}
